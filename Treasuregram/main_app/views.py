34c13d4bb6fdd52f7752ebb8d624d88346522f63
bodhibrdr <julie.gumerman@gmail.com>
2020-05-26 20:08:30 -0600

    view added

--- Treasuregram/main_app/views.py
+++ Treasuregram/main_app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse('<h1>The science is just to keep spinning <br>because the big bang is only just beginning <br> and sometimes it is all that we can do <br> just to hang on</h1>')
