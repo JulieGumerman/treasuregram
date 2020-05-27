b7a2bfaf0ba2fbd2660b8ca91855997f0be8c72e
bodhibrdr <julie.gumerman@gmail.com>
2020-05-26 20:19:43 -0600

    reconfigure urls

--- Treasuregram/Treasuregram/urls.py
+++ Treasuregram/Treasuregram/urls.py
@@ -14,10 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from main_app import views
+from django.conf.urls import include, url
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('index/', views.index )
+    url('admin/', admin.site.urls),
+    url('', include('main_app.urls'))
 ]
