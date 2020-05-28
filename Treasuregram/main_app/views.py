b2fdbd32a246c62c3e90fdcb15e2d8c64bf4e0c5
bodhibrdr <julie.gumerman@gmail.com>
2020-05-27 20:36:49 -0600

    add some bootstrap...

--- Treasuregram/main_app/views.py
+++ Treasuregram/main_app/views.py
@@ -3,4 +3,18 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+
+    return render(request, 'index.html', {'treasures': treasures})
+
+class Treasure:
+    def __init__(self, name, value, material, location):
+        self.name = name
+        self.value = value
+        self.material = material
+        self.location = location
+
+treasures = [
+    Treasure('Gold nugget', 500.00, 'gold', 'Aspen, CO'),
+    Treasure('Iron pyrite', 2.00, 'not real gold', 'Cripple Creek'),
+    Treasure('Coffee can', 20.00, 'tin', 'Colorado Springs, CO')
+]
