913b7161997356f93bc7f239c83ef5552bbfcb4f
bodhibrdr <julie.gumerman@gmail.com>
2020-05-29 14:25:22 -0600

    fixed routing woes

--- Treasuregram/main_app/views.py
+++ Treasuregram/main_app/views.py
@@ -1,21 +1,9 @@
 from django.shortcuts import render
+from .models import Treasure
 
 
 # Create your views here.
 def index(request):
-
+    treasures = Treasure.objects.all()
     return render(request, 'index.html', {'treasures': treasures})
 
-class Treasure:
-    def __init__(self, name, value, material, location, image):
-        self.name = name
-        self.value = value
-        self.material = material
-        self.location = location
-        self.image = image
-
-treasures = [
-    Treasure('Pumpkin', 500.00, 'organic', 'Aspen', '../static/images/jack-o-lantern-light.svg'),
-    Treasure('Iron pyrite', 2.00, 'not real gold', 'Cripple Creek', '../static/images/badger-honey-regular.svg'),
-    Treasure('Coffee can', 20.00, 'tin', 'Colorado Springs, CO', '../static/images/deer-solid.svg')
-]
